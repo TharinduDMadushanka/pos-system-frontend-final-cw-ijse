d6e029d20cf46b197c7e07200a3adfe491aba2d9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-10-14 15:36:35 +0530

    update Dashboard UI

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+        <hr />
+
+        <div className="footer-bootom">
+            <p className="footer-bottom-left">@ 2024 Tharindu Madushanka.All rights reserved.</p>
+            <div className="footer-bottom-right">
+                <p>Term of Services</p>
+                <p>Privacy Policy</p>
+                <p>Connect with me</p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,36 +1,60 @@
 import React from 'react';
 import './Dashboard.css';
-import { useNavigate } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
+import dashboard from '../../assets/dashboard.jpg';
+import { useNavigate } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
 
 const Dashboard = () => {
 
   const navigate = useNavigate();
 
-  const navCategory = ()=>{
-    navigate('/categories')
+  const navCategory= ()=>{
+    navigate('/categories');
   }
 
   return (
     <div className="dashboard">
+      <img src={dashboard} alt="Dashboard Background" />
+      <Navbar />
 
-      <Navbar/>
-
-      <div className="content">
-        <h1>Welcome to Your Dashboard</h1>
-        <p>This is where you can manage your profile and access features.</p>
-
-        <div className="btn-group">
-          <div className="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary" onClick={navCategory}>Item Category</button>
-            <button type="button" class="btn btn-primary">Middle</button>
-            <button type="button" class="btn btn-primary">Right</button>
+      <div className="cards">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          
+          <div onClick={navCategory} className="col">
+            <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">Item Categories</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+            </div>
           </div>
+
+          <div className="col">
+            <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">Items</h5>
+                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">Stocks</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
 
+      <Footer/>
+
     </div>
   );
-}
+};
 
 export default Dashboard;
