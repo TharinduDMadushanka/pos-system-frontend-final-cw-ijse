27af427f45a8b89e12d5043bcf20b63c8140d073
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-10-14 14:58:52 +0530

    update Home page UI

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Dashboard.css';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
 
 const Dashboard = () => {
 
@@ -12,14 +13,19 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard">
-      <h1>Welcome to Your Dashboard</h1>
-      <p>This is where you can manage your profile and access features.</p>
 
-      <div className="btn-group">
-        <div className="btn-group" role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-primary" onClick={navCategory}>Item Category</button>
-          <button type="button" class="btn btn-primary">Middle</button>
-          <button type="button" class="btn btn-primary">Right</button>
+      <Navbar/>
+
+      <div className="content">
+        <h1>Welcome to Your Dashboard</h1>
+        <p>This is where you can manage your profile and access features.</p>
+
+        <div className="btn-group">
+          <div className="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-primary" onClick={navCategory}>Item Category</button>
+            <button type="button" class="btn btn-primary">Middle</button>
+            <button type="button" class="btn btn-primary">Right</button>
+          </div>
         </div>
       </div>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './Home.css'
 import main_bg from '../../assets/main-bg.jpg'
+import main_bg2 from '../../assets/main-bg2.jpg'
 import Navbar from '../../components/Navbar/Navbar'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const navLogin = () =>{
+    navigate('/login')
+  }
+
   return (
     <div className='home'>
-        <Navbar/>
-        <img src={main_bg} alt="" />
+        {/* <Navbar/> */}
+
+        <h2>TDM</h2>
+
+        <img src={main_bg2} alt="" />
+
+        <h1><i>Welcome To My Shop</i></h1>
+        <p>It's Nice to meet you!</p>
+        <button onClick={navLogin}>Login</button>
+      
     </div>
   )
 }
